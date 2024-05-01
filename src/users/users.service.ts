e213cf978d15aeb0f9107ab26a99d57f4f19913b
Bryan Lontoc <bryanlontoc06@gmail.com>
2024-05-01 11:58:18 +0800

    feat(users): Implement create user logic

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './user.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  /**
+   * Create a new user
+   */
+  async create(name: string, email: string, password: string): Promise<User> {
+    const createdUser = new this.userModel({ name, email, password });
+    return await createdUser.save();
+  }
+}
