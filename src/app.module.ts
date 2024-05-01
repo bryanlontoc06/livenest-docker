85c17a6e17e4da5c9b18b74ce350741de7964e02
Bryan Lontoc <bryanlontoc06@gmail.com>
2024-05-01 11:41:40 +0800

    chore: Create users controller file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [UsersModule],
