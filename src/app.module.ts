c2e2f6f4ff340aa0f66cb62ab9fe7d62a7cc78b0
Bryan Lontoc <bryanlontoc06@gmail.com>
2024-05-01 18:17:36 +0800

    chore: Adds configuration for database url

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.taodhds.mongodb.net/${process.env.MONGO_ATLAS_DB}?retryWrites=true&w=majority&appName=Cluster0`,
     ),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
