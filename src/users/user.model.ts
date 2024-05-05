5a701c89686629fd4f4c0361a8449081a5943a24
Bryan Lontoc <bryanlontoc06@gmail.com>
2024-05-05 12:15:50 +0800

    chore(user-model): Removes versionKey in user Schema

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,10 +1,13 @@
 import * as mongoose from 'mongoose';
 
-export const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-});
+export const UserSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+  },
+  { versionKey: false },
+);
 
 export interface User extends mongoose.Document {
   name: string;
